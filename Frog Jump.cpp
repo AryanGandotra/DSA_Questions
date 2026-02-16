2cf5703010f8cee5decc527c882efd457dd6b286
Aryan Gandotra <aryan.gandotra.21cse@bmu.edu.in>
2026-02-16 20:27:28 +0530

    Update Frog Jump.cpp

--- Frog Jump.cpp
+++ Frog Jump.cpp
@@ -1,3 +1,31 @@
+// tabulation + space optimisation
+
+class Solution
+{
+public:
+    int minCost(vector<int> &height)
+    {
+        int n = height.size();
+        int prev = 0, prev2 = 0;
+
+        for (int i = 1; i < n; i++)
+        {
+            int oneStep = prev + abs(height[i] - height[i - 1]);
+            int twoStep = INT_MAX;
+            if (i > 1)
+            {
+                twoStep = prev2 + abs(height[i] - height[i - 2]);
+            }
+
+            int curr = min(oneStep, twoStep);
+            prev2 = prev;
+            prev = curr;
+        }
+
+        return prev;
+    }
+}; 
+
 // tabulation
 
 class Solution {
