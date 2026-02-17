c13a10c6fb68e35068ac2d55f030e246a9747fdd
Aryan Gandotra <aryan.gandotra.21cse@bmu.edu.in>
2026-02-17 18:25:19 +0530

    Update 198.House_Robber.cpp

--- 198.House_Robber.cpp
+++ 198.House_Robber.cpp
@@ -1,3 +1,26 @@
+// tabulation
+
+class Solution
+{
+public:
+    int rob(vector<int> &nums)
+    {
+        int n = nums.size();
+        vector<int> dp(n + 1, -1);
+        dp[0] = 0;
+        dp[1] = nums[0];
+
+        for (int i = 2; i <= n; i++)
+        {
+            int pick = nums[i - 1] + dp[i - 2];
+            int notpick = dp[i - 1];
+            dp[i] = max(pick, notpick);
+        }
+
+        return dp[n];
+    }
+};
+
 // memoization
 
 class Solution
