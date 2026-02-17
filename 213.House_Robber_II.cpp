041facb8721c1b3c0cdf7638c36325fe24b1934f
Aryan Gandotra <aryan.gandotra.21cse@bmu.edu.in>
2026-02-17 18:25:57 +0530

    Update 213.House_Robber_II.cpp

--- 213.House_Robber_II.cpp
+++ 213.House_Robber_II.cpp
@@ -1,43 +1,41 @@
 class Solution
 {
 public:
-    int dp[101];
-
-    int solve(vector<int> &nums, int n)
+    int helper(vector<int> &nums)
     {
-        if (n <= 0)
-            return 0;
-        if (n == 1)
-            return nums[0];
+        int n = nums.size();
+        vector<int> dp(n + 1, -1);
+        int prev2 = 0, prev = nums[0];
 
-        dp[0] = nums[0];
-        dp[1] = max(nums[0], nums[1]);
-
-        for (int i = 2; i < n; i++)
+        for (int i = 2; i <= n; i++)
         {
-            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i]);
+            int pick = nums[i - 1] + prev2;
+            int notpick = prev;
+            int curr = max(pick, notpick);
+            prev2 = prev;
+            prev = curr;
         }
 
-        return dp[n - 1];
+        return prev;
     }
 
     int rob(vector<int> &nums)
     {
-
         int n = nums.size();
 
         if (n == 1)
             return nums[0];
 
-        vector<int> nums1(nums.begin() + 1, nums.end());
-        vector<int> nums2(nums.begin(), nums.end() - 1);
+        vector<int> temp1, temp2;
 
-        memset(dp, 0, sizeof(dp));
-        int result1 = solve(nums1, n - 1);
+        for (int i = 0; i < n; i++)
+        {
+            if (i != 0)
+                temp1.push_back(nums[i]);
+            if (i != n - 1)
+                temp2.push_back(nums[i]);
+        }
 
-        memset(dp, 0, sizeof(dp));
-        int result2 = solve(nums2, n - 1);
-
-        return max(result1, result2);
+        return max(helper(temp1), helper(temp2));
     }
 };
