8427a3d224199b574f3bc3ea0c03c533f065b092
Aryan Gandotra <aryan.gandotra.21cse@bmu.edu.in>
2026-02-20 19:39:58 +0530

    Update 62. Unique Paths.cpp

--- 62. Unique Paths.cpp
+++ 62. Unique Paths.cpp
@@ -1,4 +1,34 @@
-// memoisation
+// memoisation - starting from m-1,n-1
+
+class Solution
+{
+public:
+    int func(int row, int col, vector<vector<int>> &dp)
+    {
+        if (row == 0 && col == 0)
+            return 1;
+
+        if (dp[row][col] != -1)
+            return dp[row][col];
+
+        int up = 0, left = 0;
+
+        if (row - 1 >= 0)
+            up = func(row - 1, col, dp);
+        if (col - 1 >= 0)
+            left = func(row, col - 1, dp);
+
+        return dp[row][col] = up + left;
+    }
+
+    int uniquePaths(int m, int n)
+    {
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return func(m - 1, n - 1, dp);
+    }
+};
+
+// memoisation - starting from 0,0
 
 class Solution
 {
