b855d633ebbcbaf62a4d5b7b418167d110b8ef98
Aryan Gandotra <aryan.gandotra.21cse@bmu.edu.in>
2026-02-21 18:20:07 +0530

    Update 64. Minimum Path Sum.cpp

--- 64. Minimum Path Sum.cpp
+++ 64. Minimum Path Sum.cpp
@@ -1,3 +1,34 @@
+// memoisation
+
+class Solution
+{
+public:
+    int getMinPathSum(int m, int n, vector<vector<int>> &grid, vector<vector<int>> &dp)
+    {
+        if (m == 0 && n == 0)
+            return grid[0][0];
+
+        if (dp[m][n] != -1)
+            return dp[m][n];
+
+        int up = INT_MAX, left = INT_MAX;
+
+        if (m - 1 >= 0)
+            up = grid[m][n] + getMinPathSum(m - 1, n, grid, dp);
+        if (n - 1 >= 0)
+            left = grid[m][n] + getMinPathSum(m, n - 1, grid, dp);
+
+        return dp[m][n] = min(up, left);
+    }
+
+    int minPathSum(vector<vector<int>> &grid)
+    {
+        int m = grid.size(), n = grid[0].size();
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return getMinPathSum(m - 1, n - 1, grid, dp);
+    }
+}; 
+
 // recursive - will fail in LC submission
 
 class Solution
