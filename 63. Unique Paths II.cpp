fe7c773778131c2fa5f9450f037f4316a27a367f
Aryan Gandotra <aryan.gandotra.21cse@bmu.edu.in>
2026-02-21 17:04:12 +0530

    Update 63. Unique Paths II.cpp

--- 63. Unique Paths II.cpp
+++ 63. Unique Paths II.cpp
@@ -1,3 +1,50 @@
+// tabulation
+class Solution
+{
+public:
+    int uniquePathsWithObstacles(vector<vector<int>> &obstacleGrid)
+    {
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+
+        if (obstacleGrid[0][0] == 1)
+            return 0;
+
+        for (int currRow = 0; currRow < m; currRow++)
+        {
+            for (int currCol = 0; currCol < n; currCol++)
+            {
+
+                if (currRow >= 0 && currCol >= 0 &&
+                    obstacleGrid[currRow][currCol] == 1)
+                {
+                    dp[currRow][currCol] = 0;
+                }
+                else if (currRow == 0 && currCol == 0)
+                {
+                    dp[currRow][currCol] = 1;
+                }
+                else
+                {
+                    int up = 0, left = 0;
+
+                    if (currRow - 1 >= 0)
+                        up = dp[currRow - 1][currCol];
+
+                    if (currCol - 1 >= 0)
+                        left = dp[currRow][currCol - 1];
+
+                    dp[currRow][currCol] = up + left;
+                }
+            }
+        }
+
+        return dp[m - 1][n - 1];
+    }
+};
+
 // memoisation
 
 class Solution
