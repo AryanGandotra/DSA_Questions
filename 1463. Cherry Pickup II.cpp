7657395e2c3610845dafd0af811e267c870c874b
Aryan Gandotra <aryan.gandotra.21cse@bmu.edu.in>
2026-02-22 17:15:33 +0530

    Update 1463. Cherry Pickup II.cpp

--- 1463. Cherry Pickup II.cpp
+++ 1463. Cherry Pickup II.cpp
@@ -1,3 +1,60 @@
+// tabulation
+class Solution
+{
+public:
+    int cherryPickup(vector<vector<int>> &grid)
+    {
+        int m = grid.size(), n = grid[0].size();
+        vector<vector<vector<int>>> dp(
+            m, vector<vector<int>>(n, vector<int>(n, 0)));
+
+        for (int i = m - 1; i >= 0; i--)
+        {
+            for (int j = n - 1; j >= 0; j--)
+            {
+                for (int k = n - 1; k >= 0; k--)
+                {
+                    if (i == m - 1)
+                    {
+                        if (j == k)
+                            dp[i][j][k] = grid[i][j];
+                        else
+                            dp[i][j][k] = grid[i][j] + grid[i][k];
+                    }
+                    else
+                    {
+                        int result = -1e8;
+
+                        for (int delta1 = -1; delta1 <= 1; delta1++)
+                        {
+                            for (int delta2 = -1; delta2 <= 1; delta2++)
+                            {
+                                int newJ = j + delta1;
+                                int newK = k + delta2;
+
+                                if (newJ >= 0 && newJ < n &&
+                                    newK >= 0 && newK < n)
+                                {
+
+                                    if (j == k)
+                                        result = max(result,
+                                                     grid[i][j] + dp[i + 1][newJ][newK]);
+                                    else
+                                        result = max(result,
+                                                     grid[i][j] + grid[i][k] +
+                                                         dp[i + 1][newJ][newK]);
+                                }
+                            }
+                        }
+                        dp[i][j][k] = result;
+                    }
+                }
+            }
+        }
+        return dp[0][0][n - 1];
+    }
+};
+
 // memoisation
 
 class Solution
