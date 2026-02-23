77936f731f58aac080a88305fce6e338ae65ccfc
Aryan Gandotra <aryan.gandotra.21cse@bmu.edu.in>
2026-02-23 20:38:16 +0530

    Update Subset Sum Equal To K.cpp

--- Subset Sum Equal To K.cpp
+++ Subset Sum Equal To K.cpp
@@ -1,3 +1,31 @@
+// tabulation
+#include <bits/stdc++.h> 
+bool subsetSumToK(int n, int k, vector<int> &nums){
+    vector<vector<bool>> dp(n,vector<bool>(k+1,false));
+
+    for(int i=0;i<n;i++){
+        dp[i][0]=true;
+    }
+    
+    if(nums[0] <= k)
+        dp[0][nums[0]] = true;
+
+
+    for(int i=1;i<n;i++){
+        for(int j=1;j<=k;j++){
+                bool take = false;
+                if (j >= nums[i])
+                    take = dp[i-1][j - nums[i]];
+
+                bool notTake = dp[i-1][j];
+
+                dp[i][j] = take || notTake;
+        }
+    }
+    return dp[n-1][k];
+}
+
+// memoisation
 #include <bits/stdc++.h>
 
 bool GetSubArrays(int index, vector<int> &nums, int k, vector<vector<int>> &dp)
