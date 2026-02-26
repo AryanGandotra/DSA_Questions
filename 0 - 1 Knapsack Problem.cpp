a8a14638fb739849eb90b606647a1ede58b52405
Aryan Gandotra <aryan.gandotra.21cse@bmu.edu.in>
2026-02-26 19:39:24 +0530

    Update 0 - 1 Knapsack Problem.cpp

--- 0 - 1 Knapsack Problem.cpp
+++ 0 - 1 Knapsack Problem.cpp
@@ -1,3 +1,32 @@
+// tabulation
+class Solution
+{
+public:
+    int knapsack(int W, vector<int> &val, vector<int> &wt)
+    {
+        int n = val.size();
+        vector<vector<int>> dp(n, vector<int>(W + 1, 0));
+
+        for (int j = wt[0]; j <= W; j++)
+            dp[0][j] = val[0];
+
+        for (int i = 1; i < n; i++)
+        {
+            for (int j = 0; j <= W; j++)
+            {
+                int pick = INT_MIN;
+                if (wt[i] <= j)
+                {
+                    pick = val[i] + dp[i - 1][j - wt[i]];
+                }
+                int notpick = dp[i - 1][j];
+                dp[i][j] = max(pick, notpick);
+            }
+        }
+        return dp[n - 1][W];
+    }
+};
+
 // memoisation
 class Solution
 {
