f73476b315d92b31c0d6c8de9adc1efb9f91cb52
Aryan Gandotra <aryan.gandotra.21cse@bmu.edu.in>
2026-02-27 20:57:34 +0530

    Update 322.Coin_Change.cpp

--- 322.Coin_Change.cpp
+++ 322.Coin_Change.cpp
@@ -1,3 +1,39 @@
+// tabulation
+
+class Solution
+{
+public:
+    int coinChange(vector<int> &coins, int amount)
+    {
+        int n = coins.size();
+        vector<vector<int>> dp(n, vector<int>(amount + 1, 0));
+
+        for (int j = 0; j <= amount; j++)
+        {
+            if (j % coins[0] == 0)
+                dp[0][j] = j / coins[0];
+            else
+                dp[0][j] = 1e9;
+        }
+
+        for (int i = 1; i < n; i++)
+        {
+            for (int j = 0; j <= amount; j++)
+            {
+                int pick = 1e8;
+                if (coins[i] <= j)
+                    pick = 1 + dp[i][j - coins[i]];
+
+                int notpick = dp[i - 1][j];
+
+                dp[i][j] = min(pick, notpick);
+            }
+        }
+
+        return dp[n - 1][amount] >= 1e8 ? -1 : dp[n - 1][amount];
+    }
+};
+
 // memoisation
 class Solution
 {
@@ -35,11 +71,6 @@ public:
         return ans;
     }
 };
-
-
-
-
-
 
 class Solution
 {
