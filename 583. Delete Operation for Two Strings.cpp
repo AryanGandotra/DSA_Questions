43f1ab91af93208a4eef2a02c7072f0dbfc9ab50
Aryan Gandotra <aryan.gandotra.21cse@bmu.edu.in>
2026-03-03 19:00:17 +0530

    Create 583. Delete Operation for Two Strings.cpp

---
+++ 583. Delete Operation for Two Strings.cpp
@@ -0,0 +1,34 @@
+class Solution
+{
+public:
+    int lcs(string &s1, string &s2)
+    {
+        int m = s1.length();
+        int n = s2.length();
+
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1));
+
+        for (int i = 1; i <= m; i++)
+        {
+            for (int j = 1; j <= n; j++)
+            {
+                if (s1[i - 1] == s2[j - 1])
+                {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                }
+                else
+                {
+                    dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+
+    int minDistance(string word1, string word2)
+    {
+        int lcsN = lcs(word1, word2);
+        return word1.length() + word2.length() - (2 * lcsN);
+    }
+};
