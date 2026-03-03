f83def74be720b1fee93181c00cc6856ba21d7a0
Aryan Gandotra <aryan.gandotra.21cse@bmu.edu.in>
2026-03-03 18:48:26 +0530

    Update 1312.MinimumInsertionStepstoMakeaStringPalindrome.cpp

--- 1312.MinimumInsertionStepstoMakeaStringPalindrome.cpp
+++ 1312.MinimumInsertionStepstoMakeaStringPalindrome.cpp
@@ -1,3 +1,37 @@
+// memoisation
+class Solution
+{
+public:
+    int lcs(string s1, string s2)
+    {
+        int n = s1.length(), m = n;
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= m; j++)
+            {
+                if (s1[i - 1] == s2[j - 1])
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                else
+                {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[n][m];
+    }
+
+    int minInsertions(string s)
+    {
+        string s2 = s;
+        reverse(s2.begin(), s2.end());
+        int lcsN = lcs(s, s2);
+        return s.length() - lcsN;
+    }
+};
+
 class Solution
 {
 public:
