49a3d32c6fc02b19d8971aff5dbaafbcb6fa0b38
Aryan Gandotra <aryan.gandotra.21cse@bmu.edu.in>
2026-03-06 19:36:01 +0530

    Update 115. Distinct Subsequences.cpp

--- 115. Distinct Subsequences.cpp
+++ 115. Distinct Subsequences.cpp
@@ -1,3 +1,35 @@
+// tabulation + space optimisation
+
+class Solution
+{
+public:
+    int numDistinct(string s, string t)
+    {
+        int n = s.length(), m = t.length();
+        vector<double> dp(m + 1, 0), curr(m + 1, 0);
+        dp[0] = curr[0] = 1;
+
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= m; j++)
+            {
+                if (s[i - 1] == t[j - 1])
+                {
+                    curr[j] = dp[j - 1] + dp[j];
+                }
+                else
+                {
+                    curr[j] = dp[j];
+                }
+            }
+            dp = curr;
+        }
+        return (int)dp[m];
+    }
+};
+
+// tabulation
+
 class Solution
 {
 public:
