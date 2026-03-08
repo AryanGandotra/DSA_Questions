cd4818428d7702058fb61ce22ab7ac1d5e1a9901
Aryan Gandotra <aryan.gandotra.21cse@bmu.edu.in>
2026-03-08 15:21:49 +0530

    Update 72. Edit Distance.cpp

--- 72. Edit Distance.cpp
+++ 72. Edit Distance.cpp
@@ -1,3 +1,48 @@
+// tabulation
+class Solution
+{
+public:
+    int minDistance(string word1, string word2)
+    {
+        int m = word1.length(), n = word2.length();
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+
+        for (int i = 0; i <= m; i++)
+        {
+            dp[i][0] = i;
+        }
+
+        for (int j = 0; j <= n; j++)
+        {
+            dp[0][j] = j;
+        }
+
+        for (int i = 1; i <= m; i++)
+        {
+            for (int j = 1; j <= n; j++)
+            {
+                if (word1[i - 1] == word2[j - 1])
+                    dp[i][j] = dp[i - 1][j - 1];
+                else
+                {
+                    // insert
+                    int insert = 1 + dp[i][j - 1];
+                    // replace
+                    int replace = 1 + dp[i - 1][j - 1];
+                    // delete
+                    int del = 1 + dp[i - 1][j];
+
+                    dp[i][j] = min(insert, min(replace, del));
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+};
+
+// memeoisation
+
 class Solution
 {
 public:
