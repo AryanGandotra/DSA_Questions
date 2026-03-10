9e665bd6096c19781d04cb08cbaa0e739891f9f8
Aryan Gandotra <aryan.gandotra.21cse@bmu.edu.in>
2026-03-10 18:24:11 +0530

    Update 121.Best_Time_to_Buy_and_Sell_Stock.cpp

--- 121.Best_Time_to_Buy_and_Sell_Stock.cpp
+++ 121.Best_Time_to_Buy_and_Sell_Stock.cpp
@@ -3,6 +3,25 @@ class Solution
 public:
     int maxProfit(vector<int> &prices)
     {
+        int mini = prices[0];
+        int profit = 0;
+
+        for (int i = 0; i < prices.size(); i++)
+        {
+            int cost = prices[i] - mini;
+            profit = max(profit, cost);
+            mini = min(mini, prices[i]);
+        }
+
+        return profit;
+    }
+};
+
+class Solution
+{
+public:
+    int maxProfit(vector<int> &prices)
+    {
         int ans = 0;
         int l = 0, r = 1;
         while (r < prices.size())
