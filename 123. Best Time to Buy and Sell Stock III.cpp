849aa9b0bc84627e9c1dd1159637a1c68c1bd9be
Aryan Gandotra <aryan.gandotra.21cse@bmu.edu.in>
2026-03-11 20:03:42 +0530

    Update 123. Best Time to Buy and Sell Stock III.cpp

--- 123. Best Time to Buy and Sell Stock III.cpp
+++ 123. Best Time to Buy and Sell Stock III.cpp
@@ -1,3 +1,38 @@
+// tabulation
+class Solution
+{
+public:
+    int maxProfit(vector<int> &prices)
+    {
+        int n = prices.size();
+        vector<vector<vector<int>>> dp(
+            n + 1, vector<vector<int>>(2, vector<int>(3, 0)));
+
+        dp[n][0][0] = dp[n][0][1] = dp[n][0][2] = dp[n][1][0] = dp[n][1][1] =
+            dp[n][1][2] = 0;
+
+        for (int i = n - 1; i >= 0; i--)
+        {
+            for (int j = 0; j <= 1; j++)
+            {
+                for (int k = 1; k >= 0; k--)
+                {
+                    long profit = 0;
+                    if (j == 1)
+                        profit = max((-prices[i] + dp[i + 1][0][k]), dp[i + 1][1][k]);
+                    else
+                        profit = max((prices[i] + dp[i + 1][1][k + 1]), dp[i + 1][0][k]);
+
+                    dp[i][j][k] = profit;
+                }
+            }
+        }
+
+        return dp[0][1][0];
+    }
+};
+
+// memoisation
 class Solution
 {
 public:
