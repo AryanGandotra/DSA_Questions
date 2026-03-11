388cf48ddb8c8a29a9d6f42ce2c12f5bcbebf77a
Aryan Gandotra <aryan.gandotra.21cse@bmu.edu.in>
2026-03-11 14:58:36 +0530

    Update 122.Best_Time_to_Buy_and_Sell_Stock_II.cpp

--- 122.Best_Time_to_Buy_and_Sell_Stock_II.cpp
+++ 122.Best_Time_to_Buy_and_Sell_Stock_II.cpp
@@ -1,27 +1,60 @@
-// memoisation
-class Solution {
+// tabulation
+
+class Solution
+{
 public:
-    long func(int index,int canBuy,vector<int>& prices,vector<vector<int>> &dp){
-        if(index==prices.size())
+    int maxProfit(vector<int> &prices)
+    {
+        int n = prices.size();
+        vector<vector<int>> dp(n + 1, vector<int>(2, 0));
+
+        dp[n][0] = dp[n][1] = 0;
+
+        for (int i = n - 1; i >= 0; i--)
+        {
+            for (int j = 0; j <= 1; j++)
+            {
+                long profit = 0;
+                if (j == 1)
+                    profit = max((-prices[i] + dp[i + 1][0]), dp[i + 1][1]);
+                else
+                    profit = max((prices[i] + dp[i + 1][1]), dp[i + 1][0]);
+
+                dp[i][j] = profit;
+            }
+        }
+
+        return dp[0][1];
+    }
+};
+
+// memoisation
+class Solution
+{
+public:
+    long func(int index, int canBuy, vector<int> &prices, vector<vector<int>> &dp)
+    {
+        if (index == prices.size())
             return 0;
-        
-        if(dp[index][canBuy]!=-1)
+
+        if (dp[index][canBuy] != -1)
             return dp[index][canBuy];
 
-        long profit=0;
+        long profit = 0;
 
-        if(canBuy==1)
-            profit = max((-prices[index] + func(index+1,0,prices,dp)),func(index+1,1,prices,dp));
-        else 
-            profit = max((prices[index] + func(index+1,1,prices,dp)),func(index+1,0,prices,dp));
+        if (canBuy == 1)
+            profit = max((-prices[index] + func(index + 1, 0, prices, dp)), func(index + 1, 1, prices, dp));
+        else
+            profit = max((prices[index] + func(index + 1, 1, prices, dp)), func(index + 1, 0, prices, dp));
 
-        return dp[index][canBuy]=profit;
+        return dp[index][canBuy] = profit;
     }
 
-    int maxProfit(vector<int>& prices) {
-        int canBuy=1,index=0;
-        vector<vector<int>> dp(prices.size(),vector<int>(2,-1));
-        return func(index,canBuy,prices,dp);
+    int maxProfit(vector<int> &prices)
+    {
+        int canBuy = 1, index = 0;
+        vector<vector<int>> dp(prices.size(), vector<int>(2, -1));
+        return func(index, canBuy, prices, dp);
     }
 };
 
