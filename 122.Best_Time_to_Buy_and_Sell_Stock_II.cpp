6ce7a685d38ab677db146eb556998f94bbbd2445
Aryan Gandotra <aryan.gandotra.21cse@bmu.edu.in>
2026-03-11 14:46:40 +0530

    Update 122.Best_Time_to_Buy_and_Sell_Stock_II.cpp

--- 122.Best_Time_to_Buy_and_Sell_Stock_II.cpp
+++ 122.Best_Time_to_Buy_and_Sell_Stock_II.cpp
@@ -1,3 +1,30 @@
+// memoisation
+class Solution {
+public:
+    long func(int index,int canBuy,vector<int>& prices,vector<vector<int>> &dp){
+        if(index==prices.size())
+            return 0;
+        
+        if(dp[index][canBuy]!=-1)
+            return dp[index][canBuy];
+
+        long profit=0;
+
+        if(canBuy==1)
+            profit = max((-prices[index] + func(index+1,0,prices,dp)),func(index+1,1,prices,dp));
+        else 
+            profit = max((prices[index] + func(index+1,1,prices,dp)),func(index+1,0,prices,dp));
+
+        return dp[index][canBuy]=profit;
+    }
+
+    int maxProfit(vector<int>& prices) {
+        int canBuy=1,index=0;
+        vector<vector<int>> dp(prices.size(),vector<int>(2,-1));
+        return func(index,canBuy,prices,dp);
+    }
+};
+
 class Solution
 {
 public:
