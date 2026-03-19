f0df92d5cdf5e144a1670a0cadbf3f9c7e3c165d
Aryan Gandotra <aryan.gandotra.21cse@bmu.edu.in>
2026-03-19 16:21:29 +0530

    Update 3.Longest_Substring_Without_Repeating_Characters.cpp

--- 3.Longest_Substring_Without_Repeating_Characters.cpp
+++ 3.Longest_Substring_Without_Repeating_Characters.cpp
@@ -3,6 +3,35 @@ class Solution
 public:
     int lengthOfLongestSubstring(string s)
     {
+        int ans = INT_MIN, n = s.length();
+
+        if (n == 0)
+            return n;
+
+        for (int i = 0; i < n; i++)
+        {
+            vector<int> freq(256, 0);
+            for (int j = i; j < n; j++)
+            {
+                if (freq[s[j]] == 0)
+                {
+                    freq[s[j]]++;
+                    ans = max(ans, j - i + 1);
+                }
+                else
+                    break;
+            }
+        }
+
+        return ans;
+    }
+};
+
+class Solution
+{
+public:
+    int lengthOfLongestSubstring(string s)
+    {
         int currLength = 0, maxLength = 0;
         map<char, int> mp;
         int i = 0, j = 0;
