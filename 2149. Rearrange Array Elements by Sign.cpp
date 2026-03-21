28f159cce6ff4f9ed5a6f94a9d6e54e981f9521b
Aryan Gandotra <aryan.gandotra.21cse@bmu.edu.in>
2026-03-21 19:30:33 +0530

    Update 2149. Rearrange Array Elements by Sign.cpp

--- 2149. Rearrange Array Elements by Sign.cpp
+++ 2149. Rearrange Array Elements by Sign.cpp
@@ -1,3 +1,33 @@
+// optimal
+
+class Solution
+{
+public:
+    vector<int> rearrangeArray(vector<int> &nums)
+    {
+        vector<int> ans(nums.size(), 0);
+        int posCounter = 0, negCounter = 0;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] > 0)
+            {
+                ans[2 * posCounter] = nums[i];
+                posCounter++;
+            }
+            else
+            {
+                ans[2 * negCounter + 1] = nums[i];
+                negCounter++;
+            }
+        }
+
+        return ans;
+    }
+};
+
+// Brute force
+
 class Solution
 {
 public:
