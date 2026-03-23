33eeddaedee0f3e6b2040bbec85a94958c803977
Aryan Gandotra <aryan.gandotra.21cse@bmu.edu.in>
2026-03-23 18:34:03 +0530

    Update 48. Rotate Image.cpp

--- 48. Rotate Image.cpp
+++ 48. Rotate Image.cpp
@@ -3,6 +3,32 @@ class Solution
 public:
     void rotate(vector<vector<int>> &matrix)
     {
+        int n = matrix.size();
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (i <= j)
+                    swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n / 2; j++)
+            {
+                swap(matrix[i][j], matrix[i][n - j - 1]);
+            }
+        }
+    }
+};
+
+class Solution
+{
+public:
+    void rotate(vector<vector<int>> &matrix)
+    {
         vector<vector<int>> copy = matrix;
 
         int n = matrix.size() - 1, m = matrix.size();
