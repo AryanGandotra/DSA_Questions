b4842da3ac40f4d9514507d67c3bbffd20fe516d
Aryan Gandotra <aryan.gandotra.21cse@bmu.edu.in>
2026-03-28 16:21:43 +0530

    Update 75.Sort_Colors.cpp

--- 75.Sort_Colors.cpp
+++ 75.Sort_Colors.cpp
@@ -3,6 +3,35 @@ class Solution
 public:
     void sortColors(vector<int> &nums)
     {
+        int n = nums.size();
+        int low = 0, mid = 0, high = n - 1;
+
+        while (mid <= high)
+        {
+            if (nums[mid] == 0)
+            {
+                swap(nums[mid], nums[low]);
+                low++;
+                mid++;
+            }
+            else if (nums[mid] == 1)
+            {
+                mid++;
+            }
+            else
+            {
+                swap(nums[mid], nums[high]);
+                high--;
+            }
+        }
+    }
+};
+
+class Solution
+{
+public:
+    void sortColors(vector<int> &nums)
+    {
         int count[3] = {0};
 
         for (int i = 0; i < nums.size(); i++)
