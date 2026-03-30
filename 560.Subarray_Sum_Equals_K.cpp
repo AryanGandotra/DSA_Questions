6e6b36f73934270cb113e96bfe2f1b8da93435d1
Aryan Gandotra <aryan.gandotra.21cse@bmu.edu.in>
2026-03-30 18:35:59 +0530

    Update 560.Subarray_Sum_Equals_K.cpp

--- 560.Subarray_Sum_Equals_K.cpp
+++ 560.Subarray_Sum_Equals_K.cpp
@@ -1,3 +1,27 @@
+class Solution
+{
+public:
+    int subarraySum(vector<int> &nums, int k)
+    {
+        int prefixSum = 0;
+        int count = 0;
+        map<int, int> hashmap;
+        hashmap[0] = 1;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            prefixSum += nums[i];
+            if (hashmap.find(prefixSum - k) != hashmap.end())
+            {
+                count += hashmap[prefixSum - k];
+            }
+            hashmap[prefixSum]++;
+        }
+
+        return count;
+    }
+};
+
 // brute force approach
 
 class Solution
