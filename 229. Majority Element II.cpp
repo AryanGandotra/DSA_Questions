4c50051ae9994c47d9976a965e04f236da31bf5b
Aryan Gandotra <aryan.gandotra.21cse@bmu.edu.in>
2026-03-31 18:31:31 +0530

    Update 229. Majority Element II.cpp

--- 229. Majority Element II.cpp
+++ 229. Majority Element II.cpp
@@ -1,4 +1,4 @@
-//  brute force
+// better
 
 class Solution
 {
@@ -19,6 +19,38 @@ public:
         {
             if (element.second > (n / 3))
                 ans.push_back(element.first);
+        }
+
+        return ans;
+    }
+};
+
+//  brute force
+
+class Solution
+{
+public:
+    vector<int> majorityElement(vector<int> &nums)
+    {
+        int n = nums.size();
+        vector<int> ans;
+
+        for (int i = 0; i < n; i++)
+        {
+            int count = 0;
+            for (int j = i; j < n; j++)
+            {
+                if (nums[j] == nums[i])
+                    count++;
+                if (count > (n / 3))
+                {
+                    if (find(ans.begin(), ans.end(), nums[i]) == ans.end())
+                    {
+                        ans.push_back(nums[i]);
+                    }
+                    break;
+                }
+            }
         }
 
         return ans;
