1991e2943e53d66dc56093293ffa2534414b5377
Aryan Gandotra <aryan.gandotra.21cse@bmu.edu.in>
2026-03-31 19:17:33 +0530

    Update 229. Majority Element II.cpp

--- 229. Majority Element II.cpp
+++ 229. Majority Element II.cpp
@@ -1,3 +1,60 @@
+// optimised
+class Solution
+{
+public:
+    vector<int> majorityElement(vector<int> &nums)
+    {
+        int cnt1 = 0, cnt2 = 0, element1 = -1, element2 = -1, n = nums.size();
+        vector<int> ans;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (cnt1 == 0 && nums[i] != element2)
+            {
+                cnt1++;
+                element1 = nums[i];
+            }
+            else if (cnt2 == 0 && nums[i] != element1)
+            {
+                cnt2++;
+                element2 = nums[i];
+            }
+
+            else if (nums[i] == element1)
+            {
+                cnt1++;
+            }
+            else if (nums[i] == element2)
+            {
+                cnt2++;
+            }
+            else
+            {
+                cnt1--;
+                cnt2--;
+            }
+        }
+
+        cnt1 = 0, cnt2 = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (nums[i] == element1)
+                cnt1++;
+
+            else if (nums[i] == element2)
+                cnt2++;
+        }
+
+        if (cnt1 > n / 3)
+            ans.push_back(element1);
+        if (cnt2 > n / 3)
+            ans.push_back(element2);
+        sort(ans.begin(), ans.end());
+        return ans;
+    }
+};
+
 // better
 
 class Solution
