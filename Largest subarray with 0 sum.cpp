9b4ab526dff7c2f258857fd91b6387728f864f46
Aryan Gandotra <aryan.gandotra.21cse@bmu.edu.in>
2026-03-31 19:56:13 +0530

    Update Largest subarray with 0 sum.cpp

--- Largest subarray with 0 sum.cpp
+++ Largest subarray with 0 sum.cpp
@@ -1,3 +1,33 @@
+// better
+
+class Solution
+{
+public:
+    int maxLength(vector<int> &arr)
+    {
+        int maxLen = 0, sum = 0;
+        unordered_map<int, int> sumIndexMap;
+
+        for (int i = 0; i < arr.size(); i++)
+        {
+            sum += arr[i];
+            if (sum == 0)
+            {
+                maxLen = i + 1;
+            }
+            else if (sumIndexMap.find(sum) != sumIndexMap.end())
+            {
+                maxLen = max(maxLen, i - sumIndexMap[sum]);
+            }
+            else
+            {
+                sumIndexMap[sum] = i;
+            }
+        }
+        return maxLen;
+    }
+};
+
 // brute force - will Give TLE
 
 class Solution
@@ -5,17 +35,19 @@ class Solution
 public:
     int maxLength(vector<int> &arr)
     {
-        int ans = 0, sum = 0;
-        map<int, int> hashmap;
+        int ans = 0;
 
         for (int i = 0; i < arr.size(); i++)
         {
-            int currlength = 0;
-            sum += arr[i];
-            if (hashmap.find(-sum) != hashmap.end())
+            int sum = 0, currlength = 0;
+            for (int j = i; j < arr.size(); j++)
             {
-                currlength = i + 1;
-                ans = max(currlength, ans);
+                sum += arr[j];
+                currlength = j - i + 1;
+                if (sum == 0)
+                {
+                    ans = max(currlength, ans);
+                }
             }
         }
         return ans;
