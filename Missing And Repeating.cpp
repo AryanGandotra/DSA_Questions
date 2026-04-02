2c0bd1ba6dbf8e86b3df453d417ded230b35506f
Aryan Gandotra <aryan.gandotra.21cse@bmu.edu.in>
2026-04-02 17:58:31 +0530

    Update Missing And Repeating.cpp

--- Missing And Repeating.cpp
+++ Missing And Repeating.cpp
@@ -1,3 +1,38 @@
+// Optimal
+
+class Solution
+{
+public:
+    vector<int> findTwoElement(vector<int> &arr)
+    {
+        long long n = arr.size();
+
+        long long sumofFirstNnumbers = (n * (n + 1)) / 2;
+        long long sumofSquaresofFirstNnumbers = (n * (n + 1) * (2 * n + 1)) / 6;
+
+        long long sumofArray = 0;
+        long long sumofSquaresArray = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            sumofArray += arr[i];
+            sumofSquaresArray += (long long)arr[i] * arr[i];
+        }
+
+        long long xminusy = sumofArray - sumofFirstNnumbers;
+        long long second = sumofSquaresArray - sumofSquaresofFirstNnumbers;
+
+        long long xplusy = second / xminusy;
+
+        long long x = (xminusy + xplusy) / 2;
+        long long y = xplusy - x;
+
+        return {(int)x, (int)y};
+    }
+};
+
+// better
+
 class Solution
 {
 public:
