292da97c89b89b7d37ef3761be36270fa0dea660
Aryan Gandotra <aryan.gandotra.21cse@bmu.edu.in>
2026-04-05 18:32:26 +0530

    Update 704.Binary_Search.cpp

--- 704.Binary_Search.cpp
+++ 704.Binary_Search.cpp
@@ -1,3 +1,34 @@
+// recursive
+class Solution
+{
+public:
+    int binarySearch(int low, int high, vector<int> &nums, int target)
+    {
+        if (low > high)
+            return -1;
+
+        int mid = (low + high) / 2;
+        if (nums[mid] == target)
+            return mid;
+        else if (target < nums[mid])
+        {
+            return binarySearch(low, mid - 1, nums, target);
+        }
+        else
+        {
+            return binarySearch(mid + 1, high, nums, target);
+        }
+        return -1;
+    }
+
+    int search(vector<int> &nums, int target)
+    {
+        int low = 0, high = nums.size() - 1;
+        return binarySearch(low, high, nums, target);
+    }
+};
+
+// iterative
 class Solution
 {
 public:
@@ -26,7 +57,7 @@ public:
         {
             return -1;
         }
-        
+
         return mid;
     }
 };
