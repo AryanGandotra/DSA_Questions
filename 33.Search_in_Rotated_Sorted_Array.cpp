321849d6e1ba17dd5df793e886213fe600670df0
Aryan Gandotra <aryan.gandotra.21cse@bmu.edu.in>
2026-04-26 14:41:33 +0530

    Update 33.Search_in_Rotated_Sorted_Array.cpp

--- 33.Search_in_Rotated_Sorted_Array.cpp
+++ 33.Search_in_Rotated_Sorted_Array.cpp
@@ -1,44 +1,28 @@
-class Solution
-{
+class Solution {
 public:
-    int search(vector<int> &nums, int target)
-    {
-        int low = 0;
-        int high = nums.size() - 1;
+    int search(vector<int>& nums, int target) {
+        int low = 0, high = nums.size() - 1;
 
-        int mid = low + (high - low) / 2;
+        while (low <= high) {
+            int mid = (low + high) / 2;
 
-        while (low < high)
-        {
             if (nums[mid] == target)
                 return mid;
+
+            // identify the sorted half
+
+            // left half sorted
             if (nums[low] <= nums[mid])
-            {
-                if (target > nums[mid] || target < nums[low])
-                    low = mid + 1;
-                else
-                {
+                if (nums[low] <= target && target <= nums[mid])
                     high = mid - 1;
-                }
-            }
+                else
+                    low = mid + 1;
+            // right half sorted
             else
-            {
-                if (target < nums[mid] || target > nums[high])
-                {
-                    high = mid - 1;
-                }
-                else
-                {
+                if (nums[mid] <= target && target <= nums[high])
                     low = mid + 1;
-                }
-            }
-
-            mid = low + (high - low) / 2;
-        }
-
-        if (nums[mid] == target)
-        {
-            return mid;
+                else
+                    high = mid - 1;
         }
 
         return -1;
