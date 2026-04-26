3022aaefc26665fb5fe6142639b9366c98091478
Aryan Gandotra <aryan.gandotra.21cse@bmu.edu.in>
2026-04-26 14:57:42 +0530

    Update 81.Search_in_Rotated_Sorted_Array_II.cpp

--- 81.Search_in_Rotated_Sorted_Array_II.cpp
+++ 81.Search_in_Rotated_Sorted_Array_II.cpp
@@ -1,46 +1,36 @@
-class Solution
-{
+class Solution {
 public:
-    bool search(vector<int> &arr, int k)
-    {
-        int low = 0, high = arr.size() - 1;
-        while (low <= high)
-        {
+    bool search(vector<int>& nums, int target) {
+        int low = 0, high = nums.size() - 1;
+
+        while (low <= high) {
             int mid = (low + high) / 2;
 
-            if (arr[mid] == k)
-                return 1;
+            if (nums[mid] == target)
+                return true;
 
-            if (arr[low] == arr[mid] && arr[mid] == arr[high])
-            {
-                low++;
-                high--;
+            if(nums[low]==nums[mid] && nums[mid]==nums[high]){
+                low=low+1;
+                high=high-1;
                 continue;
             }
 
-            if (arr[low] <= arr[mid])
-            {
-                if (arr[low] <= k && k <= arr[mid])
-                {
+            // identify the sorted half
+
+            // left half sorted
+            if (nums[low] <= nums[mid])
+                if (nums[low] <= target && target <= nums[mid])
                     high = mid - 1;
-                }
                 else
-                {
                     low = mid + 1;
-                }
-            }
+            // right half sorted
             else
-            {
-                if (arr[mid] <= k && k <= arr[high])
-                {
+                if (nums[mid] <= target && target <= nums[high])
                     low = mid + 1;
-                }
                 else
-                {
                     high = mid - 1;
-                }
-            }
         }
-        return 0;
+
+        return false;
     }
 };
